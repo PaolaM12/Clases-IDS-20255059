f37aa2135c1d45b74b5496f106dba4dfd5b057a1
PaolaM12 <menendezpaola09@gmail.com>
2025-11-29 07:19:30 -0600

    Mi primer script

---
+++ Mi_Primer_Script.py
@@ -0,0 +1,7 @@
+# este programa funcionaba con Python 3.13.7
+variable1 = 100
+variable2 = 150
+variable3 = variable1 + variable2 # esto no se usará, esto es un comentario de linea
+print(variable3)
+"""este es un comentario multilinea,
+todo lo que esta aqui no se ejecutara"""
