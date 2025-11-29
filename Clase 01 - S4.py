2b5972aecd9dafc455ec3bc9bac1fe6479cebe83
PaolaM12 <menendezpaola09@gmail.com>
2025-11-29 07:13:33 -0600

    Add files via upload
    Clases - Semana 4

---
+++ Clase 01 - S4.py
@@ -0,0 +1,39 @@
+usuario = 'Paola'
+cantidad_alumnos = 74
+media_edad = 18.23524
+monto_hope = 1234.5678
+inversion_evento = -9875.21548
+
+'''print (type(cantidad_alumnos))
+print(type(media_edad))
+print(type(cantidad_alumnos)is int)
+print(type(media_edad)is int)'''
+
+'''print('el usuario es',usuario, 'y tiene',cantidad_alumnos, 'pajaritos en su aula')
+print('y la edad promedio es de', media_edad)
+
+print(f'el usuario es {usuario}')
+print (f'y tiene una cantidad de {cantidad_alumnos -4} pajaritos')
+print (f'con edad promedio de {media_edad:.2f} años')
+print (f'y recolectaron ${monto_hope:,.2f} como un donativo')
+print (f'y la totalidad de gastos fue de $ {abs(inversion_evento):,.2f} dolares.')
+
+print(type(usuario) is str)
+
+esta_lloviendo = False
+print(type(esta_lloviendo))
+print (type(monto_hope))'''
+
+'''nombre = 'Paola'
+apellido = 'Menéndez'
+nombre_completo = nombre + ' ' + apellido
+print (nombre_completo)'''
+
+nuevas_inscripciones = 0
+cantidad_alumnos = 60
+nuevas_inscripciones = int(input('nuevos alumnos:'))
+nombre_profe = 'Alvin'
+nombre_profe = input ('Ingrese el nombre del profesor: ')
+print(type(nombre_profe))
+print(nombre_profe)
+print(cantidad_alumnos + nuevas_inscripciones)
---
+++ Clase 02 - S4.py
@@ -0,0 +1,15 @@
+'''Itemingresar = input('Ingrese el item: ')
+print(f'El item ingresado es {Itemingresar}')'''
+
+'''my_string = 'ABCDE'
+letra = int(input('indique el indice de la letra a mostrar (del 0 al 4): '))
+print(my_string[letra])'''
+
+my_string= 'ABCDEFG'
+#print(len(my_string))
+#print(my_string[0:4]) #subsetting por slices
+
+#print('H' not in my_string)
+
+#print('A'*8) # para repetir los carácteres
+
